39160154486d831ef3b9d547140e59087a51b20e
Himanshu Pandey <himanshu21pandey@gmail.com>
2021-09-09 13:34:09 +0530

    Delete and Conditional Component and Display

--- src/App.js
+++ src/App.js
@@ -22,11 +22,17 @@ const App = () => {
         day: 'Feb 5th at 2.30 PM',
         reminder: false
     },
-])
+  ])
+
+// Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} /> : 'No Tasks To Show'}
     </div>
   )
 }
